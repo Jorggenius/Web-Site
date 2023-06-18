43441205d91a435cd69aea4699610d97a04b6c2a
jorge luis <martinezmonajorgeluis@gmail.com>
2023-06-18 18:22:24 -0500

    In this code I practiced my skills in JS and CSS and offcourse HTML

---
+++ functions.js
@@ -0,0 +1,5 @@
+const pay = () =>{
+    alert("GANASTE")
+}
+
+export default pay 
---
+++ indexJS.js
@@ -0,0 +1,6 @@
+let buttonPay = document.querySelector(".button-pay");
+buttonPay.addEventListener("click", pay)
+// import pay from "./functions"
+function pay(){
+    alert("GANASTE")
+}
